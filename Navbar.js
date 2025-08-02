927b57e8f45bd7834bb5ea090a1fcfc84e790bae
CHOWDARY SARITHA <142637859+Chowdarysaritha311@users.noreply.github.com>
2025-08-02 21:37:09 +0530

    Navbar.js

---
+++ Navbar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Navbar = () => (
+  <nav className="bg-indigo-600 text-white p-4 text-center text-xl font-bold">
+    Aptitude & Reasoning Practice
+  </nav>
+);
+export default Navbar;

bd8d34435de605c11383e78b1d7143dd775ff59e
CHOWDARY SARITHA <142637859+Chowdarysaritha311@users.noreply.github.com>
2025-08-02 21:36:13 +0530

    questions.js

---
+++ questions.js
@@ -0,0 +1,20 @@
+const questions = [
+  {
+    id: 1,
+    question: "What is the next number in the series: 2, 4, 8, 16, ?",
+    options: [32, 24, 20, 18],
+    answer: 32
+  },
+  {
+    id: 2,
+    question: "If A = 1, B = 2, ..., what is the value of CAT?",
+    options: [24, 27, 27, 30],
+    answer: 24
+  },
+  {
+    id: 3,
+    question: "A train 150m long runs at 45km/hr. How long does it take to cross a pole?",
+    options: [12, 18, 6, 15],
+    answer: 12
+  }
+];
